b74e916bf2cad05dff030ce18a7d598fb33a3199
Maxence Bonamy <maxencebonamy@gmail.com>
2023-12-28 18:12:18 +0100

    Refactor TaskFactory to use static arrays for possible task names and descriptions

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -23,27 +23,4 @@ class Task extends Model
         'project_id'
     ];
 
-    public static function possibleTaskNames()
-    {
-        return [
-            "Rédiger un plan d'action",
-            'Planifier la réunion avec les collaborateurs',
-            'Convoquer Jean Yves',
-            'Virer Camille du projet',
-            "Déployer l'application",
-            "Installer Laravel"
-        ];
-    }
-
-    public static function possibleTaskDescription()
-    {
-        return [
-            "Il faut que nous avançions plus vite",
-            'Les collaborateurs à prévenir sont : Jean Mich et Yves',
-            'Il faut aussi préparer des arguments pour virer Jean Yves',
-            'Il faudrait la convoquer la semaine prochaine',
-            "L'application doit être terminée la semaine prochaine",
-            "Laravel version 1.0.0.0.2"
-        ];
-    }
 }
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -11,6 +11,24 @@ use App\Models\Task;
  */
 class TaskFactory extends Factory
 {
+    protected static array $possibleTaskNames = [
+        "Rédiger un plan d'action",
+        'Planifier la réunion avec les collaborateurs',
+        'Convoquer Jean Yves',
+        'Virer Camille du projet',
+        "Déployer l'application",
+        "Installer Laravel"
+    ];
+
+    protected static array $possibleTaskDescription = [
+        "Il faut que nous avançions plus vite",
+        'Les collaborateurs à prévenir sont : Jean Mich et Yves',
+        'Il faut aussi préparer des arguments pour virer Jean Yves',
+        'Il faudrait la convoquer la semaine prochaine',
+        "L'application doit être terminée la semaine prochaine",
+        "Laravel version 1.0.0.0.2"
+    ];
+
     /**
      * Define the model's default state.
      *
@@ -19,8 +37,8 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->randomElement(Task::possibleTaskNames()),
-            'description' => fake()->randomElement(Task::possibleTaskDescription()),
+            'name' => fake()->randomElement(static::$possibleTaskNames),
+            'description' => fake()->randomElement(static::$possibleTaskDescription),
             'deadline'=> fake()->dateTimeBetween('now','+3 months'),
             'status' => fake()->numberBetween(0,3),
             'priority' => fake()->numberBetween(0,2),
