ee2b48e4a2f6a200a44b69d2488dfeebaab37473
HERBRETEAU Enzo <enzo.herbreteau@reseau.eseo.fr>
2023-12-29 18:22:52 +0100

    Adding test for task post in database

--- tests/Feature/testPostTaskController.php
+++ tests/Feature/testPostTaskController.php
@@ -8,11 +8,12 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class testPostTaskController extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Teste la bonne attribution des variables avec le controlleur
      */
     public function test_create_task(): void
     {
@@ -36,5 +37,31 @@ class testPostTaskController extends TestCase
         $this->assertEquals(0, $result['status']);
         $this->assertEquals(1, $result['priority']);
         $this->assertEquals(Project::all()->first()->id, $result['project_id']);
+        Task::find($result['id'])->delete();
+    }
+
+    /**
+     * Teste la création de la tâche dans la BDD avec le controlleur Post
+     */
+    public function test_create_database_task(): void
+    {
+        $taskPostController = new TaskController();
+
+        //Creation de la requête
+        $requestData = [
+            'name' => 'Test2',
+            'description' => 'Description of the task test2',
+            'deadline' => '2023-12-31',
+            'priority' => 1,
+            'project_id' => Project::all()->first()->id
+        ];
+
+        $request = new Request($requestData);
+
+        $result = json_decode($taskPostController->post($request), true);
+        $this->assertNotNull(Task::find($result['id']));
+        if($task = Task::find($result['id'])){
+            $task->delete();
+        }
     }
 }
