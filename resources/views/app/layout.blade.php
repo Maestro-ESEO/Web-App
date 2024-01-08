343fb6ffed8c7f61c9e5887c6c1bf01e779f59d7
Maxence Bonamy <maxencebonamy@gmail.com>
2024-01-08 17:59:17 +0100

    Update layout.blade.php with new styles and logo

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -3,14 +3,21 @@
 
 @section("page")
 
-<section class="w-64 h-full flex items-center justify-center shadow-box">
-    <form action={{ route('auth.logout') }} method="post">
+<section class="w-64 p-6 h-screen flex flex-col items-center justify-between shadow-box">
+    <div class="w-full flex items-center justify-center gap-2">
+        <img src="logo.png" alt="Logo" class="w-8">
+        <h2 class="text-2xl text-blue font-bold">Maestro</h2>
+    </div>
+
+    <form action={{ route('auth.logout') }} method="post" class="w-full flex justify-start">
         @csrf
-        <button>Logout</button>
+        <button class="flex items-center gap-2 text-dark-gray">
+            <x-icons.logout /> Logout
+        </button>
     </form>
 </section>
 
-<main class="h-full flex flex-1 flex-col items-center justify-center">
+<main class="min-h-full flex flex-1 flex-col items-center justify-center">
     @yield("content")
 </main>
 
