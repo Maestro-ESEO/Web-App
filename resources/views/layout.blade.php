5e8dcb7bc09b45c3afcc64c40ea28ba9c6ec66f1
HERBRETEAU Enzo <enzo.herbreteau@reseau.eseo.fr>
2024-01-08 17:58:26 +0100

    Update dashboard and layout styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,7 +26,8 @@
                     boxShadow: {
                         "box": "0 0 60px #838383",
                         "btn": "0 4px 4px rgba(131, 131, 131, 0.5)",
-                        "input": "inset 0 4px 4px rgba(131, 131, 131, 0.5)"
+                        "input": "inset 0 4px 4px rgba(131, 131, 131, 0.5)",
+                        "project" : "0 0 10px rgba(0,0,0,0.5)"
                     },
                     fontFamily: {
                         "sans": ["Poppins", "sans-serif"]
