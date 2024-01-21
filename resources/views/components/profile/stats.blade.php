0e6555309702071c641e1e918ab864a40ceba52a
Maxence Bonamy <maxencebonamy@gmail.com>
2024-01-21 16:11:24 +0100

    Update profile stats layout

--- resources/views/components/profile/stats.blade.php
+++ resources/views/components/profile/stats.blade.php
@@ -11,15 +11,21 @@ $nbAssignedTasks = app(TaskController::class)->getAssignedTasksCount();
 <div class="flex flex-1 flex-col items-center justify-start gap-3 rounded-2xl shadow-project p-4 min-w-96 bg-gray-100">
     <h3 class="font-bold text-black text-xl">Your statistics</h3>
 
-    <div class="">
-        <div>
-            <x-icons.project />
-            <h3>{{ $nbAssignedProjects }}</h3>
+    <div class="w-full flex-1 flex flex-row items-center justify-evenly">
+        <div class="flex flex-col items-center justify-start gap-2">
+            <x-icons.project size=64 />
+            <p class="flex flex-row items-center justify-center gap-2">
+                <span class="text-4xl font-bold">{{ $nbAssignedProjects }}</span>
+                assigned projects
+            </p>
         </div>
 
-        <div>
-            <x-icons.task />
-            <h3>{{ $nbAssignedTasks }}</h3>
+        <div class="flex flex-col items-center justify-start gap-2">
+            <x-icons.task size=64 />
+            <p class="flex flex-row items-center justify-center gap-2">
+                <span class="text-4xl font-bold">{{ $nbAssignedTasks }}</span>
+                assigned tasks
+            </p>
         </div>
     </div>
 </div>
